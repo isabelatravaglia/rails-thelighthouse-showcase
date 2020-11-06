181a5bb06de3b0e152df0fa32895815f20d0dae8
isabela santos <isabelatravaglia@gmail.com>
2020-11-06 18:34:09 +0000

    added comments to the prod_push file and added it to gitignore.

--- prod_push.sh
+++ prod_push.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-
+# Gets full image tag in docker.compose.yml
 full_tag=$(grep -i "image: rails" ./docker-compose.yml)
+
+#Gets only tag number
 tag_number=$(echo $full_tag | egrep -o '(([0-9][\.])|[0-9])(([0-9][\.])|[0-9])(([0-9][\.])|[0-9])')
 
 echo $full_tag
 echo $tag_number
 
+#Builds image
+docker-compose build
+
+#Tag image
 docker tag rails-thelighthouse:"$tag_number" registry.heroku.com/discoverthelighthouse/web
 
+#Push tagged image to Heroku
 docker push registry.heroku.com/discoverthelighthouse/web
 
+#Releases 
 heroku container:release web
