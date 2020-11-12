01856b3784cabe99c60a1a0521f1b12d9dbe7093
isabela santos <isabelatravaglia@gmail.com>
2020-11-12 17:47:37 +0000

    made changes to allow the contact modal to work correctly. The modal disappears after a validation error, which is not the best CX. Will have to make an AJAX request and also change the flash alert to a nicer one. Emails cannot be sent yet. Added 2 options on docker-compose file to allow the interaction with the container. This was made to allow debugging interaction, but in the end it was not necessary since the command docker-compose run --service-ports web allows debugging without changes on the docker-composer file.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,18 +2,21 @@ class MessagesController < ApplicationController
 	skip_before_action :authenticate_user!, only: [ :new, :create ]
 
 	def new
-    @message = Message.new
-  end
+		@message = Message.new
+	end
 
 	def create
-		@message = Message.new(message_params)
+    @message = Message.new(message_params)
 		if @message.valid?
-				MessageMailer.contact(@message).deliver_now
-				redirect_to new_message_url
-				flash[:notice] = "Recebemos sua mensagem! Entraremos em conto brevemente!"
+			MessageMailer.contact(@message).deliver_now
+			redirect_to new_message_url
+			flash[:notice] = "Recebemos sua mensagem! Entraremos em contato brevemente!"
 		else
-				flash[:notice] = "Ops! Não conseguimos receber sua mensagem. Por favor tente novamente"
-				render :new
+			flash[:notice] = "Ops! Não conseguimos receber sua mensagem. Por favor preencha todos os campos e tente novamente."
+      # render :new
+      # render "/messages/_new_rerender", message: @message
+      #have to use ajax
+      redirect_to root_path
 		end
 	end
 
