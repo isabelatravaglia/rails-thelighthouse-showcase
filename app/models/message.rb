6200f19cf2422c9a9611fc0e2e7ec678fdf797e9
isabela santos <isabelatravaglia@gmail.com>
2020-11-16 18:45:00 +0000

    added styling to the contact modal. Added a nickname field to be hidden with CSS and which is supposed to be blank. This strategy is to avoid robots to use to form to spam.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,8 +1,9 @@
 class Message
   include ActiveModel::Model
-  attr_accessor :name, :email, :body
+  attr_accessor :name, :email, :body, :nickname
 
   validates :name, :body, :email, presence: true
+  validates :nickname, presence: false
   validates :email, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i,
   message: "Por favor, digite um email válido." }
 end
