a1b8cc00e30e2529a6be3d711f809f9f7abe3f39
isabela santos <isabelatravaglia@gmail.com>
2021-01-14 19:35:40 +0000

    added a privacy_policy on message.rb with validation. Added a checkbok on the contact form with the Privacy Policy link.

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,9 +1,10 @@
 class Message
   include ActiveModel::Model
-  attr_accessor :name, :email, :body, :nickname, :company, :phone
+  attr_accessor :name, :email, :body, :nickname, :company, :phone, :privacy_policy
 
   validates :name, :body, :email, presence: { message: "É necessário preencher o nome" }
   validates :nickname, absence: { message: "Esse campo deve ficar vazio" }
   validates :email, format: { with: /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i,
   message: "Por favor, digite um email válido." }
+  validates :privacy_policy, acceptance: { message: 'Por favor, aceite nossa Política de Privacidade para nos enviar uma mensagem.' }
 end
