e12d2def7aee8fbf35e693b4f92681c0cfd8de10
isabela santos <isabelatravaglia@gmail.com>
2021-02-22 18:26:29 +0000

    added local storage to the service model (service: :local). Changed service images reference to get them from the assets pipeline instead of Cloudinary  - url(<%= asset_path service.photo.filename %> -.

--- app/models/service.rb
+++ app/models/service.rb
@@ -3,5 +3,5 @@ class Service < ApplicationRecord
     has_many :audience_services, dependent: :destroy
     has_many :partners, through: :partner_services
     has_many :audiences, through: :audience_services
-    has_one_attached :photo
+    has_one_attached :photo, service: :local
 end
