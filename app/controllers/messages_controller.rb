c954b828c1cc1401ef51cb4934455d02bf5dd117
isabela santos <isabelatravaglia@gmail.com>
2021-02-24 14:36:39 +0000

    added bootstrap alert html into a modal so that it could be rendered by create.js.erb. Now the flash message shows up, albeit with a weird behavior on mobile. Should check it later.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,21 +3,8 @@ class MessagesController < ApplicationController
   invisible_captcha only: [:create], honeypot: :nickname
 
   def new
-		@message = Message.new
-	end
-
-  # def create
-  #   @message = Message.new(message_params)
-  #   if @message.valid?
-	# 		ContactMailer.contact(@message).deliver_now
-	# 		redirect_back(fallback_location: root_path)
-	# 		flash[:notice] = "Recebemos sua mensagem! Entraremos em contato brevemente!"
-	# 	else
-	# 		flash[:notice] = "Ops! Não conseguimos receber sua mensagem. Por favor, preencha todos os campos obrigatórios e tente novamente."
-  #     # render "/messages/_new", locals: {message: @message}
-  #     redirect_back(fallback_location: root_path)
-  #   end
-	# end
+    @message = Message.new
+  end
 
   def create
     @message = Message.new(message_params)
@@ -25,14 +12,13 @@ class MessagesController < ApplicationController
       if @message.valid?
         ContactMailer.contact(@message).deliver_now
         format.js { render 'create.js.erb' }
-        flash[:notice] = "Recebemos sua mensagem! Entraremos em contato brevemente!"
       else
         format.js { render 'error.js.erb' }
       end
     end
-	end
+  end
 
-	private
+  private
 
 	def message_params
     params.require(:message).permit(:name, :email, :body, :nickname, :company, :phone, :privacy_policy)
