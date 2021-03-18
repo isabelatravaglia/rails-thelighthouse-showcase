6b88c6ce16b41799a418106099beb1a8026c84bd
isabela santos <isabelatravaglia@gmail.com>
2021-03-18 16:26:02 +0000

    added an article-card.scss. Added banner--article to banner (still need to change the picture). Added @months to articles controller so that the months on the articles card displays the correct abbreviated month names in Portuguese. Added a title validation of uniqueness on article model so that there are no conflicts on article links (as they are constructed based on the article title). Added Article index view. Added ids for the team members on the About page so that the partner (author) link in the article cardscan lead to the partner that wrote the article. Modified seeds to incorporate more articles.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,7 @@ class ArticlesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :index, :show ]
   def index
     @articles = Article.all
+    @months = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun", "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
   end
 
   def show
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, uniqueness: true
   belongs_to :partner
   has_many :article_categorizations, dependent: :destroy
   has_many :article_categories, through: :article_categorizations, dependent: :destroy
