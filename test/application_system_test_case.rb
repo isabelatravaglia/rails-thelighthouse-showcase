8383fb0faf4e6804af885bdd2591b6b396f93f2d
isabela santos <isabelatravaglia@gmail.com>
2021-04-22 19:35:34 +0000

    fixed an image reference on Article show page. Added parallelization of 1 to application system test. Added the title_param to the second system test so that the article could be created.

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,6 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  parallelize(workers: 1)
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: {url: ENV['HUB_URL']}
   
   Capybara.app_host = "http://#{IPSocket.getaddress(Socket.gethostname)}"
--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -10,22 +10,14 @@ class ArticlesTest < ApplicationSystemTestCase
     login_as users(:george)
     visit "/articles/new"
     # save_and_open_screenshot
-    # attach_file('article_photo', 'app/assets/images/life-coaching.jpg')
-    # attach_file('article_photo', Rails.root.join('app','assets','images','life-coaching.jpg'))
-    # attach_file('article_photo', File.join(Rails.root,'app/assets/images/life-coaching.jpg'))
-    fill_in "article_title", with: "Le Wagon"
-    find(".trix-content").set("Change your life: Learn to code")
+    fill_in "article_title", with: "Conheça a The Lighthouse"
+    find(".trix-content").set("Nosso propósito é te inspirar.")
     check 'carreira'
-    
-    # find('form input[type="file"]')
-    # save_and_open_screenshot
-
     click_on 'Criar Artigo'
-    # save_and_open_screenshot
 
     # Should be redirected to Home with new product
-    # assert_equal article_path, page.current_path
-    # assert_text "Change your life: Learn to code"
+    assert_equal article_path(title_param: "conheca-a-the-lighthouse"), page.current_path
+    assert_text "Nosso propósito é te inspirar."
     # save_and_open_screenshot
   end
 end
