b02640e71d2379050b7a7216f92ec3553d7bf3f8
isabela santos <isabelatravaglia@gmail.com>
2021-04-23 16:28:25 +0000

    inserted an env variable on docker-compose to make sure tests run sequentially to avoid connection errors on Selenium container. Since we are working with a single Selenium container, if tests run in parallel they'll fail. Added 2 news tests: make sure logged users can only create articles if they are admins and make sure that unsigned users cannot create an article.

--- test/system/articles_test.rb
+++ test/system/articles_test.rb
@@ -1,15 +1,14 @@
 require "application_system_test_case"
 
 class ArticlesTest < ApplicationSystemTestCase
-  # test "viewing the index" do
-  #   visit articles_path
-  #   assert_selector "h1", text: "Inspire-se"
-  # end
+  test "viewing the index" do
+    visit articles_path
+    assert_selector "h1", text: "Inspire-se"
+  end
 
   test "lets a signed in user with admin privileges create a new article" do
     login_as users(:george)
     visit "/articles/new"
-    # save_and_open_screenshot
     fill_in "article_title", with: "Conheça a The Lighthouse"
     find(".trix-content").set("Nosso propósito é te inspirar.")
     check 'carreira'
@@ -19,5 +18,17 @@ class ArticlesTest < ApplicationSystemTestCase
     assert_equal article_path(title_param: "conheca-a-the-lighthouse"), page.current_path
     assert_text "Nosso propósito é te inspirar."
     # save_and_open_screenshot
+  end
+
+  test "doesnt let signed in users without admin priviledges to create an article" do
+    login_as users(:maria)
+    visit "/articles/new"
+    assert_text "Pundit::NotAuthorizedError"
+    # save_and_open_screenshot
+  end
+
+  test "doesnt let unsigned users to create an article" do
+    visit "/articles/new"
+    assert_text "You need to sign in or sign up before continuing"
   end
 end
