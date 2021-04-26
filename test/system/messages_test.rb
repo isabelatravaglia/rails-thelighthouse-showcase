20ce606bde96c39ba99aba7eae8506483f01f95a
isabela santos <isabelatravaglia@gmail.com>
2021-04-26 20:07:54 +0000

    added fixtures for interest modal in proposito for services and leaders pages. Changed the way to target the interest modal's btn so that the test wouldn't find 2 elements with the same reference. Changed vscode theme on devcontainer json.

--- test/system/messages_test.rb
+++ test/system/messages_test.rb
@@ -13,10 +13,22 @@ class MessagesTest < ApplicationSystemTestCase
     assert_text "Recebemos sua mensagem! Entraremos em contato brevemente!"
     # save_and_open_screenshot
   end
+  ############################ Lideranca #######################################
+  test "anyone can send an interest message regarding the course Lideranca Inspiradora at Services Page" do
+    visit services_path
+    find('a[data-target="#liderancaContactUsModal"]').click
+    fill_in "message_name", with: "Testing interest message - Lideranca Inspiradora"
+    fill_in "message_email", with: "testing-lideranca-inspiradora@hotmail.com"
+    fill_in "message_body", with: "Testing interest message - Lideranca Inspiradora"
+    check "message_privacy_policy"
+    click_button "Enviar"
+    assert_text "Recebemos sua mensagem! Entraremos em contato brevemente!"
+    # save_and_open_screenshot
+  end
 
   test "anyone can send an interest message regarding the course Lideranca Inspiradora at Leaders Page" do
     visit leaders_path
-    click_link_or_button "Eu Quero!"
+    find('a[data-target="#liderancaContactUsModal"]').click
     fill_in "message_name", with: "Testing interest message - Lideranca Inspiradora"
     fill_in "message_email", with: "testing-lideranca-inspiradora@hotmail.com"
     fill_in "message_body", with: "Testing interest message - Lideranca Inspiradora"
@@ -27,8 +39,8 @@ class MessagesTest < ApplicationSystemTestCase
   end
 
   test "anyone can send an interest message regarding the course Lideranca Inspiradora at Companies Page" do
-    visit leaders_path
-    click_link_or_button "Eu Quero!"
+    visit companies_path
+    find('a[data-target="#liderancaContactUsModal"]').click
     fill_in "message_name", with: "Testing interest message - Lideranca Inspiradora"
     fill_in "message_email", with: "testing-lideranca-inspiradora@hotmail.com"
     fill_in "message_body", with: "Testing interest message - Lideranca Inspiradora"
@@ -37,25 +49,25 @@ class MessagesTest < ApplicationSystemTestCase
     assert_text "Recebemos sua mensagem! Entraremos em contato brevemente!"
     # save_and_open_screenshot
   end
-
-  test "anyone can send an interest message regarding the course The Workshop at Leaders Page" do
-    visit leaders_path
-    click_link_or_button "Eu Quero!"
-    fill_in "message_name", with: "Testing interest message - Lideranca Inspiradora"
-    fill_in "message_email", with: "testing-lideranca-inspiradora@hotmail.com"
-    fill_in "message_body", with: "Testing interest message - Lideranca Inspiradora"
+############################ Proposito #######################################
+  test "anyone can send an interest message regarding the course Proposito at Services Page" do
+    visit services_path
+    find('a[data-target="#workshopContactUsModal"]').click
+    fill_in "message_name", with: "Testing interest message - Proposito"
+    fill_in "message_email", with: "testing-proposito@hotmail.com"
+    fill_in "message_body", with: "Testing interest message - Proposito"
     check "message_privacy_policy"
     click_button "Enviar"
     assert_text "Recebemos sua mensagem! Entraremos em contato brevemente!"
     # save_and_open_screenshot
   end
 
-  test "anyone can send an interest message regarding the course The Workshop at Companies Page" do
+  test "anyone can send an interest message regarding the course Proposito at Leaders Page" do
     visit leaders_path
-    click_link_or_button "Eu Quero!"
-    fill_in "message_name", with: "Testing interest message - Lideranca Inspiradora"
-    fill_in "message_email", with: "testing-lideranca-inspiradora@hotmail.com"
-    fill_in "message_body", with: "Testing interest message - Lideranca Inspiradora"
+    find('a[data-target="#workshopContactUsModal"]').click
+    fill_in "message_name", with: "Testing interest message - Proposito"
+    fill_in "message_email", with: "testing-proposito@hotmail.com"
+    fill_in "message_body", with: "Testing interest message - Proposito"
     check "message_privacy_policy"
     click_button "Enviar"
     assert_text "Recebemos sua mensagem! Entraremos em contato brevemente!"
