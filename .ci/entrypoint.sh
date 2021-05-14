4551044f83b6eef72ae3935cc38fd302a8aaf5a9
isabela santos <isabelatravaglia@gmail.com>
2021-05-14 21:48:08 +0000

    added new commands to entrypoint to make sure db config is being replaced

--- .ci/entrypoint.sh
+++ .ci/entrypoint.sh
@@ -8,7 +8,23 @@ for file in $PWD/*; do
   echo "${file##*/}"
 done
 
+echo "replacing database config"
 cp config/database.github.yml config/database.yml
+echo "finished replacing database config"
+
+echo "entering config path"
+cd config
+
+echo "listing files on config path"
+for file in $PWD/*; do
+  echo "${file##*/}"
+done
+
+echo "moving back to parent folder"
+cd ..
+
+echo "running db:prepare"
 bundle exec rails db:prepare
+echo "finished running db:prepare"
 
 exec "$@"
