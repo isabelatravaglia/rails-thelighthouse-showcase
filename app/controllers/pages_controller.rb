ebea792d43fe3d7ce0d1bde722899a6436126dfd
isabela santos <isabelatravaglia@gmail.com>
2021-07-15 20:30:44 +0000

    added permission for unlogged users to see the privacy policy page. Added tests for privacy-policy, about, leaders and companies pages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :about, :companies, :leaders ]
+  skip_before_action :authenticate_user!, only: [ :home, :about, :companies, :leaders, :privacy_policy ]
 
   def home
     @services = Service.where(hp_show: true)
--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -20,4 +20,24 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get "/"
     assert_select "h2", "Depoimentos"
   end
+
+  test "should show private policy page" do
+    get "/privacy-policy"
+    assert_select "h1", "Política de Privacidade"
+  end
+
+  test "should show about page" do
+    get "/about"
+    assert_select "h1", "Quem somos"
+  end
+
+  test "should show leaders page" do
+    get "/leaders"
+    assert_select "h1", "Alinhe-se ao que faz sentido para si"
+  end
+
+  test "should show companies page" do
+    get "/companies"
+    assert_select "h1", "Cuide dos que cuidam dos seus clientes"
+  end
 end
