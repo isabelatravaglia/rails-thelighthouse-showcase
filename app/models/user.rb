70a276f9a3f83a7f07afe4de9e1b3bd0c76d8f25
isabela santos <isabelatravaglia@gmail.com>
2021-07-24 19:14:26 +0000

    downgraded sprockets gem to 3.7.2 from 4.0.2 due to a segment fault error. Removed user first and last name presence validation to make sign up easier. In the future will need to add this validation back and add the first and last name fields to the sign up form. Added requirements on rails_admin.rb initializer to avoid a production error when pushing to Heroku. Updated image tag.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   validates :email, uniqueness: true, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  # validates :first_name, presence: true
+  # validates :last_name, presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
